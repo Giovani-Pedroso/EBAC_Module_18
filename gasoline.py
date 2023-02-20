ca4b0a8f9092d25e424ecf972ca169fda05eb861
Giovani-Pedroso <a>
2023-02-20 19:22:06 -0300

    addition of the title and lables to the plot

--- gasoline.py
+++ gasoline.py
@@ -1,10 +1,11 @@
-import seaborn as sns
 import pandas as pd
+import seaborn as sns
 
 gas_df = pd.read_csv(filepath_or_buffer="gasolina.csv")
-#gas_df.head()
+gas_df.head() 
 
 gas_plot = sns.lineplot(data=gas_df, x="dia", y="venda")
+gas_plot.set(xlabel = 'Dia', ylabel = 'Venda', title="Venda de gasolina por dia")
 
 fig = gas_plot.get_figure()
-fig.savefig('gasolina.png')  
+fig.savefig('gasolina.png') 
